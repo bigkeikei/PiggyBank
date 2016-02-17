84658fe939c4af8f8dbb8283ed9223a6f8a1acf7
Patrick Liu <patrick.liu@kerrylogistics.com>
2016-02-17 19:22:55 +0800

    Add untracked files

---
+++ PiggyBank.UnitTesting/Mocks/MockSimpleIdentityDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.Entity;
+using SimpleIdentity.Models;
+using System.Threading.Tasks;
+
+namespace SimpleIdentity.UnitTesting.Mocks
+{
+    public class MockSimpleIdentityDbContext : Mock.MockDbContext, ISimpleIdentityDbContext
+    {
+        public MockSimpleIdentityDbContext()
+        {
+            Data = new MockData();
+            Init();
+        }
+
+        public MockSimpleIdentityDbContext(MockData data)
+        {
+            Data = data;
+            Init();
+        }
+
+        private void Init()
+        {
+            Users = GetMockDbSet(Data.Users).Object;
+            Tokens = GetMockDbSet(Data.Tokens).Object;
+            Authorizations = GetMockDbSet(Data.Authorizations).Object;
+            Clients = GetMockDbSet(Data.Clients).Object;
+
+            SaveCount = 0;
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+        public DbSet<Authorization> Authorizations { get; set; }
+        public DbSet<Client> Clients { get; set; }
+
+        public MockData Data { get; }
+        public int SaveCount { get; private set; }
+
+        public int SaveChanges() { SaveCount++; return 1; }
+        public Task<int> SaveChangesAsync() { return Task.FromResult(SaveChanges()); }
+    }
+}
---
+++ src/SimpleIdentity/Models/Managers/UserManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Security;
+
+namespace SimpleIdentity.Models
+{
+    public class UserManager : IUserManager
+    {
+        private ISimpleIdentityDbContext _dbContext;
+
+        public UserManager(ISimpleIdentityDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> CreateUser(User user)
+        {
+            if (user == null) throw new SimpleIdentityDataException("User object is missing");
+            user.Authentication = new UserAuthentication { User = user, Secret = Membership.GeneratePassword(8, 0) };
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> FindUser(int userId)
+        {
+            var q = await (from b in _dbContext.Users
+                           where b.Id == userId
+                           select b).ToListAsync();
+            if (!q.Any()) throw new SimpleIdentityDataNotFoundException("User [" + userId + "] cannot be found");
+            return q.First();
+        }
+
+        public async Task<User> FindUserByName(string userName)
+        {
+            var q = await (from b in _dbContext.Users
+                           where b.Name == userName
+                           select b).ToListAsync();
+            if (!q.Any()) throw new SimpleIdentityDataNotFoundException("User [" + userName + "] cannot be found");
+            return q.First();
+        }
+
+        public async Task<User> FindUserByToken(string accessToken)
+        {
+            var q = await _dbContext.Tokens
+                .Where(b => b.AccessToken == accessToken && 
+                    b.TokenTimeout > DateTime.Now && 
+                    b.User.IsActive)
+                .Select(b => new { User = b.User }).ToListAsync();
+            if (!q.Any()) throw new SimpleIdentityDataNotFoundException("Token [" + accessToken + "] cannot be found");
+            return q.First().User;
+        }
+
+        public async Task<User> UpdateUser(User user)
+        {
+            if (user == null) throw new SimpleIdentityDataException("User object is missing");
+            User userToUpdate = await FindUser(user.Id);
+            
+            if (userToUpdate.Name != user.Name) throw new SimpleIdentityDataException("Editing User.Name is not supported");
+            await _dbContext.SaveChangesAsync();
+            return userToUpdate;
+        }
+
+        public async Task<IEnumerable<User>> ListUsers()
+        {
+            return await _dbContext.Users.ToListAsync();
+        }
+    }
+}
